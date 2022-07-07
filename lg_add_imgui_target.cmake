513f3e05e6a897261643bb1c2e4c9fc774026bbf
Pascal Thomet <pthomet@gmail.com>
2022-07-07 18:20:17 +0200

    review lg_imgui_config.h (also handles implot declspec)

--- lg_add_imgui_target.cmake
+++ lg_add_imgui_target.cmake
@@ -4,10 +4,10 @@ set(_THIS_MODULE_BASE_DIR "${CMAKE_CURRENT_LIST_DIR}")
 # This will create an imgui target with the correct install, include and link options
 function(add_imgui_target imgui_dir)
     if(NOT TARGET imgui)
-        file(GLOB imgui_sources ${imgui_dir}/*.h ${imgui_dir}/*.cpp)
+        file(GLOB imgui_sources ${imgui_dir}/*.h ${imgui_dir}/*.cpp ${imgui_dir}/misc/cpp/*.cpp ${imgui_dir}/misc/cpp/*.h)
         add_library(imgui SHARED ${imgui_sources})
         target_include_directories(imgui PUBLIC ${imgui_dir})
-        target_compile_definitions(imgui PRIVATE IMGUI_USER_CONFIG="${_THIS_MODULE_BASE_DIR}/lg_add_imgui_target_imconfig.h")
+        target_compile_definitions(imgui PRIVATE IMGUI_USER_CONFIG="${_THIS_MODULE_BASE_DIR}/lg_imgui_imconfig.h")
 
         install(TARGETS imgui DESTINATION ./lib/)
     endif()
